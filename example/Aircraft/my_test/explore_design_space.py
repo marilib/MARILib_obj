8197e1cdb5287258409cbe74c0fc5706b9fb0d07
Micolas-enac <56388167+Micolas-enac@users.noreply.github.com>
2020-06-04 19:25:16 +0200

    clean draw_design_space again

--- example/Aircraft/my_test/explore_design_space.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dtf = pd.read_csv("explore_design.txt",sep=";")
-
-print(dtf.head())
-print(str(type(dtf.iloc[0,2])) + "  -->  " + str(dtf.iloc[0,2]))
-
-dtf.to_csv("explore_design_pandas.csv",sep=";")
